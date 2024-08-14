a2becad2bb0ac139ecc88e11842682b0c2ced8c1
Kayode Dan Precious <danielkayode222@gmail.com>
2024-08-14 11:32:45 +0100

    hero text animation

--- src/app/components/layouts/sectionLayouts/hero/index.jsx
+++ src/app/components/layouts/sectionLayouts/hero/index.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 import CustomButton from "@/app/components/globals/customButton";
 import React from "react";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   const stats = [
@@ -26,32 +29,40 @@ const Hero = () => {
         CONSULTING MARKETING DESIGN
       </p>
 
-      <div className="text-center py-5 col-center-center">
-        <div className="lg:text-[1.5rem]  flex md:w-[70%] md:text-[2rem] col-center-center text-[0.9rem] font-semibold md:leading-tight leading-[3rem]">
-          <h3 className="md:mb-[-1em]">We create exponential</h3>
-          <h1 className="lg:text-[10rem] md:text-[4em] flex  text-[4.5rem] p-3 big-text font-black">
-            gr
-            <span className=" px-2 flex items-center">
-              <div className="lg:w-[1.4em] w-[1.2em] rounded-[4em] lg:h-[.7em] h-[.6em] relative mb-[-.1em] bg-gradient flex justify-center items-center">
-                <div className="lg:w-[1em] w-[0.9em] rounded-[4em] lg:h-[.3.5em] h-[.3em]  dark:bg-black bg-white"></div>
-                <div className="h-[.5em] rounded-full w-[.5em] lg:w-[.6em] lg:h-[.6em] flex justify-center items-center left-[.5px] absolute dark:bg-black bg-white shadow-black shadow-2xl hover:translate-x-[100%] transition-all">
-                  <div className="h-[.5em] rounded-full w-[.5em] lg:w-[90%] lg:h-[90%] left-[.5px] dark:bg-black bg-white shadow-black shadow-sm"></div>
+      <motion.div
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="text-center py-5 col-center-center">
+          <div className="lg:text-[1.5rem]  flex md:w-[70%] md:text-[2rem] col-center-center text-[0.9rem] font-semibold md:leading-tight leading-[3rem]">
+            <h3 className="md:mb-[-1em]">We create exponential</h3>
+            <h1 className="lg:text-[10rem] md:text-[4em] flex  text-[4.5rem] p-3 big-text font-black">
+              gr
+              <span className=" px-2 flex items-center">
+                <div className="lg:w-[1.4em] w-[1.2em] rounded-[4em] lg:h-[.7em] h-[.6em] relative mb-[-.1em] bg-gradient flex justify-center items-center">
+                  <div className="lg:w-[1em] w-[0.9em] rounded-[4em] lg:h-[.3.5em] h-[.3em]  dark:bg-black bg-white"></div>
+                  <div className="h-[.5em] rounded-full w-[.5em] lg:w-[.6em] lg:h-[.6em] flex justify-center items-center left-[.5px] absolute dark:bg-black bg-white shadow-black shadow-2xl hover:translate-x-[100%] transition-all">
+                    <div className="h-[.5em] rounded-full w-[.5em] lg:w-[90%] lg:h-[90%] left-[.5px] dark:bg-black bg-white shadow-black shadow-sm"></div>
+                  </div>
                 </div>
-              </div>
-            </span>
-            wth
-          </h1>{" "}
-          in your business
+              </span>
+              wth
+            </h1>{" "}
+            in your business
+          </div>
         </div>
-      </div>
+      </motion.div>
 
       <div className="col-center-center my-4 py-5 md:my-0">
         <p className="text-xs md:text-sm text-center md:w-[70%] px-5">
-          <span className="text-gradient text-[1.5em]">...</span>Unlock your brand’s potential with innovative strategies and tailored
-          solutions that drive results and elevate your market presence
+          <span className="text-gradient text-[1.5em]">...</span>Unlock your
+          brand’s potential with innovative strategies and tailored solutions
+          that drive results and elevate your market presence
         </p>
         <div className="my-4 py-3">
-          <CustomButton href="/" text="Have a project?" />
+          <CustomButton href="/contactUs" text="Have a project?" />
         </div>
       </div>
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import BlockLayout from "./components/layouts/sectionLayouts/blocks";
 import BusinessDescription from "./components/layouts/sectionLayouts/businessDescription";
 import CtaSection from "./components/layouts/sectionLayouts/ctaSection";
@@ -7,26 +8,25 @@ import OurProcess from "./components/layouts/sectionLayouts/ourProcess";
 import ReviewSection from "./components/layouts/sectionLayouts/successStories";
 import TextImageLayout from "./components/layouts/sectionLayouts/textImageLayout";
 import { coreValues, whyChooseUs } from "./utils/dataStructures";
+import { motion } from "framer-motion";
 
 const LandingPage = () => {
   return (
     <section className="">
-      <Hero />
+        <Hero />
+        <BusinessDescription />
 
-      <BusinessDescription />
-
-      <BlockLayout data={coreValues} title="Our Core Values" />
-
-      <TextImageLayout
-        image='/salesDeal.webp'
-        btntext="Learn more"
-        title="Massive Sales Promotion"
-        subText="Maximize your sales potential with our dynamic promotional strategies. We craft tailored campaigns designed to capture attention, drive engagement, and convert leads into loyal customers. Whether it’s through compelling offers, targeted advertising, or strategic partnerships, our approach ensures that your promotions not only reach but resonate with your target audience"
-        position="lg:flex-row-reverse"
-      />
+        <BlockLayout data={coreValues} title="Our Core Values" />
+        <TextImageLayout
+          image="/salesDeal.webp"
+          btntext="Learn more"
+          title="Massive Sales Promotion"
+          subText="Maximize your sales potential with our dynamic promotional strategies. We craft tailored campaigns designed to capture attention, drive engagement, and convert leads into loyal customers. Whether it’s through compelling offers, targeted advertising, or strategic partnerships, our approach ensures that your promotions not only reach but resonate with your target audience"
+          position="lg:flex-row-reverse"
+        />
 
       <TextImageLayout
-        image='/training.webp'
+        image="/training.webp"
         btntext="Learn more"
         title="Incentive Programs"
         subText="Boost employee motivation and client loyalty with our innovative incentive programs. From customized rewards and recognition schemes to performance-based incentives, we design programs that align with your organizational goals and drive positive behavior. Our programs are crafted to enhance productivity, reward excellence, and foster a culture of achievement within your company"
@@ -40,7 +40,7 @@ const LandingPage = () => {
       />
 
       <TextImageLayout
-        image='/branded.webp'
+        image="/branded.webp"
         btntext="Learn more"
         title="Corporate Gifts & Branded Merchandize"
         subText="Elevate your brand presence with our premium corporate gifts and branded merchandise. We offer a wide range of high-quality products that make a lasting impression on clients and partners. Our curated selection includes everything from sophisticated gifts to practical items that enhance brand visibility and strengthen business relationships. Let us help you choose the perfect items that reflect your company’s values and make a memorable impact."
