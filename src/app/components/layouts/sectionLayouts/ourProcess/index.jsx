c0eaaa79d7fe820a716e40bf278e55529e6b3d01
Kayode Dan Precious <danielkayode222@gmail.com>
2025-11-10 06:25:17 +0100

    revamped

--- src/app/components/layouts/sectionLayouts/ourProcess/index.jsx
+++ src/app/components/layouts/sectionLayouts/ourProcess/index.jsx
@@ -12,17 +12,20 @@ const OurProcess = () => {
         return (
           <div
           key={index}
-            className={`lg:rounded-[5em] rounded-[1.5em] bg-contrastShade flex flex-col lg:flex-row lg:p-8 px-3 py-8 my-32 lg:h-[10em] shadow-lg relative ${process.position} lg:items-start items-center`}
+            className={`lg:rounded-[5em] rounded-[1.5em] bg-contrastShade flex flex-col lg:flex-row lg:p-8 px-3 py-8 my-32 lg:h-[13em] shadow-lg relative ${process.position} lg:items-start items-center`}
           >
-            <div className="lg:w-[50%] mt-8 lg:mt-0 flex flex-col justify-center mx-5 text-center lg:text-left">
+            <div className="lg:w-[50%] mt-8 lg:mt-0 flex flex-col justify-center mx-5 text-center lg:text-left h-full">
                 <p className="lg:hidden flex justify-center text-[2rem] font-bold">{process.icon}</p>
-              <p className="text-[1.2rem] lg:text-sm font-bold my-4  ">{process.title}</p>
-              <p className="lg:text-xs ">{process.text}</p>
+              <p className="text-[1.2rem] lg:text-sm font-bold my-4 lg:my-0  ">{process.title}</p>
+              <p className="lg:text-xs mt-3">{process.text}</p>
             </div>
+
+
+
             <div className={`flex lg:w-[50%] h-full ${process.position}`}>
               <div className="w-[60%]  justify-cente">
                 <div 
-                  className={`rounded-full absolute  lg:top-0 top-[-3em]  row-center-center  ${process.stagePosition}  lg:h-full lg:scale-150 bg-primaryShade text-white p-8 lg:text-[5rem] font-bold`}
+                  className={`rounded-full absolute  lg:top-[2%] bottom-[15%] top-[-3em]  row-center-center  ${process.stagePosition}  lg:h-[80%] h-[5em] lg:scale-150 bg-primaryShade text-contrastShade  p-8 lg:text-[5rem] font-bold`}
                 >
                   {process.stage}   
                 </div>
--- src/app/components/underdev/index.jsx
+++ src/app/components/underdev/index.jsx
@@ -4,12 +4,12 @@ import { FaSmileWink } from "react-icons/fa";
 
 const UnderDev = ({href, subText}) => {
   return (
-    <section className="flex justify-center items-center h-[40vh] p-4">
+    <section className="flex justify-center items-center h-[40vh] p-4 lg:mt-20 mt-10">
       <div className="text-center">
         <div className="flex justify-center">
           <FaSmileWink className="lg:text-[7rem]  text-[4rem]" />
         </div>
-        <h1 className="md:text-[5em] font-bold text-[3em] text-gradient">
+        <h1 className="md:text-[5em] font-bold text-[3em] text-gradient mt-10">
           OOPS! Page not yet Here...
         </h1>
         <p className="text-sm py-7 line-through   font-bold">
